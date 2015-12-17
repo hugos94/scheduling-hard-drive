329dc4ac2a46ac262d89ab501004b415ee1158e1
Hugo Piauilino <hugos94@gmail.com>
2015-12-17 16:04:50 -0300

    Teste de insercao de arquivo adicionada. Main comentada.

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ from sstf import *
 from elevador import *
 
 def main():
-    # Recebe o nome do arquivo do console
-    filename = sys.argv[-1]
 
-    # Inicializa o gerenciador de arquivos
-    fm = FileManager()
-    # Le o arquivo e retorna em forma de lista
-    inputs = fm.read_input(filename)
-    # Retira a quantidade de setores da lista e armazena na variavel sectors
-    sectors = inputs.pop(0)
-    # Retira a posicao inicial do cilidro e armazena na variavel initial_sector
-    initial_sector = inputs.pop(0)
+    filename = sys.argv[-1] # Recebe o nome do arquivo do console
 
-    fcfs = FCFS(sectors, initial_sector)
-    fcfs.execute(inputs)
+    if(filename == sys.argv[0]): # Verifica se algum arquivo foi recebido
+        print("Arquivo não informado!")
 
-    sstf = SSTF(sectors, initial_sector)
-    sstf.execute(inputs)
+    else:
+        fm = FileManager() # Inicializa o gerenciador de arquivos
 
-    elevador = Elevador(sectors, initial_sector)
+        inputs = fm.read_input(filename) # Le o arquivo e retorna em forma de lista
 
-    elevador.execute(inputs)
+        sectors = inputs.pop(0) # Retira a quantidade de setores da lista e armazena na variavel sectors
 
+        initial_sector = inputs.pop(0) # Retira a posicao inicial do cilidro e armazena na variavel initial_sector
+
+        fcfs = FCFS(sectors, initial_sector) # Inicializa o algoritmo FCFS
+        fcfs.execute(inputs) # Executa o algoritmo FCFS
+
+        sstf = SSTF(sectors, initial_sector) # Inicializa o algoritmo SSTF
+        sstf.execute(inputs) # Executa o algoritmo SSTF
+
+        elevador = Elevador(sectors, initial_sector) # Inicializa o algoritmo Elevador
+        elevador.execute(inputs) # Executa o algoritmo Elevador
 
 
 if __name__ == '__main__':
