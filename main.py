eae64517f838c8e815a01ab4d84b08f4b3ea233e
Hugo Piauilino <hugos94@gmail.com>
2015-12-17 12:22:32 -0300

    Realizado a leitura do arquivo.

--- main.py
+++ main.py
@@ -2,14 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from file_manager import *
+from fcfs import *
+from sstf import *
+from elevador import *
 
 def main():
+    # Recebe o nome do arquivo do console
     filename = sys.argv[-1]
-    print(filename)
+
+    # Inicializa o gerenciador de arquivos
+    fm = FileManager()
+    # Le o arquivo e retorna em forma de lista
+    inputs = fm.read_input(filename)
+    # Retira a quantidade de setores da lista e armazena na variavel sectors
+    sectors = inputs.pop(0)
+
+
 
 if __name__ == '__main__':
     main()
-
-FCFS 640
-SSTF 236
-ELEVADOR 299
